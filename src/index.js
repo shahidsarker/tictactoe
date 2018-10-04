9142b7acfd08ad7b241caa7eb9024080756bf9c5
Shahid Sarker <shahid.sarker@gmail.com>
2018-10-04 16:29:18 -0400

    winner works

--- src/index.js
+++ src/index.js
@@ -20,6 +20,9 @@ class Board extends React.Component {
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -38,9 +41,14 @@ class Board extends React.Component {
   }
 
   render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
 
-    
-    const status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
