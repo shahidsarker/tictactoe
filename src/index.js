1af8506320f817f69cec51840ba240e796bc8d9d
Shahid Sarker <shahid.sarker@gmail.com>
2018-10-04 22:36:59 -0400

    testing

--- src/index.js
+++ src/index.js
@@ -75,6 +75,18 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = "Winner: " + winner;
@@ -92,7 +104,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{desc}</ol>
         </div>
       </div>
     );
